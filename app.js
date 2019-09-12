d83b12e6936bf2e27d081a4e74002b37320b2bff
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-09-12 15:36:05 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+console.log("Starting");
+
+setTimeout(() => {
+  console.log("2 seconds has passed");
+}, 2000);
+
+setTimeout(() => {
+  console.log("0 second timer");
+}, 0);
+
+console.log("Stopping");
