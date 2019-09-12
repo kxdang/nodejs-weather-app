2920411b6344cceb9d6ab0d46655e5e0342ee5fd
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-09-12 18:55:59 -0400

    adding JSON option and setting it to true, code refactor to one line

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const request = require("request");
 
 const key = config.configAPI;
 const url = `https://api.darksky.net/forecast/${key}/37.8267,-122.4233`;
-console.log(url);
-request({ url: url }, (error, response) => {
-  const data = JSON.parse(response.body);
-  console.log(data.currently);
+
+request({ url: url, json: true }, (error, response) => {
+  console.log(response.body.currently);
 });
