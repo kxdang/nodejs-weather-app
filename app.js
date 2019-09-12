c0335c0e07853d70c8f0118867f995237eed3ab1
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-09-12 19:12:18 -0400

    accessing data from API to console.log

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const config = require("./config");
 const request = require("request");
 
 const key = config.configAPI;
-const url = `https://api.darksky.net/forecast/${key}/37.8267,-122.4233`;
+const url = `https://api.darksky.net/forecast/${key}/37.8267,-122.4233?units=si`;
 
 request({ url: url, json: true }, (error, response) => {
-  console.log(response.body.currently);
+  const currently = response.body.currently;
+  console.log(
+    ` ${response.body.daily.data[0].summary} It is currently ${currently.temperature} degrees outside. There is a ${currently.precipProbability}% chance of rain`
+  );
 });
