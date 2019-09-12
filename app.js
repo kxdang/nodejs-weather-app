db0522ed8c8a62f74a4d5ee6371e9ba83a7e539b
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-09-12 19:52:09 -0400

    adding Geocoding api, finished challenge by accessing the center property of parsed JSON

--- app.js
+++ app.js
@@ -10,3 +10,9 @@ request({ url: url, json: true }, (error, response) => {
     ` ${response.body.daily.data[0].summary} It is currently ${currently.temperature} degrees outside. There is a ${currently.precipProbability}% chance of rain`
   );
 });
+
+// Geocoding
+const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoia3hkYW5nIiwiYSI6ImNrMGhidGtrcTAwenEzcXBwZG4zcG1qcWoifQ.9UesO7UBWMh7i7IBsGybRw&limit=1`;
+request({ url: geoUrl, json: true }, (error, response) => {
+  console.log(response.body.features[0].center);
+});
