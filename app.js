42f9f6042f8d9429d785a388bf8a5ccfb496f273
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-09-14 11:36:57 -0400

    adding error handling for request for location of lat and long

--- app.js
+++ app.js
@@ -2,17 +2,31 @@ const config = require("./config");
 const request = require("request");
 
 const key = config.configAPI;
-const url = `https://api.darksky.net/forecast/${key}/37.8267,-122.4233?units=si`;
 
-request({ url: url, json: true }, (error, response) => {
-  const currently = response.body.currently;
-  console.log(
-    ` ${response.body.daily.data[0].summary} It is currently ${currently.temperature} degrees outside. There is a ${currently.precipProbability}% chance of rain`
-  );
-});
+// const url = `https://api.darksky.net/forecast/${key}/37.8267,-122.4233?units=si`;
+// request({ url: url, json: true }, (error, response) => {
+//   if (error) {
+//     console.log("Unable to connect to weather service!");
+//   } else if (response.body.error) {
+//     console.log("Unable to find location");
+//   } else {
+//     const currently = response.body.currently;
+//     console.log(
+//       `${response.body.daily.data[0].summary} It is currently ${currently.temperature} degrees outside. There is a ${currently.precipProbability}% chance of rain`
+//     );
+//   }
+// });
 
 // Geocoding
 const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoia3hkYW5nIiwiYSI6ImNrMGhidGtrcTAwenEzcXBwZG4zcG1qcWoifQ.9UesO7UBWMh7i7IBsGybRw&limit=1`;
 request({ url: geoUrl, json: true }, (error, response) => {
-  console.log(response.body.features[0].center);
+  if (error) {
+    console.log("Unable to connect to service");
+  } else if (response.body.features.length == 0) {
+    console.log("Please search another term");
+  } else {
+    const latitude = response.body.features[0].center[1];
+    const longitude = response.body.features[0].center[0];
+    console.log(latitude, longitude);
+  }
 });
