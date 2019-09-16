4ea809685a795b6b25381e1413762a27fe0b232f
ikien.dang@gmail.com <ikien.dang@gmail.com>
2019-09-16 12:21:49 -0400

    completing challenge to parse forecast data

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const config = require("./config");
 const request = require("request");
+const geocode = require("./utils/geocode");
 
 const key = config.configAPI;
 
@@ -17,42 +18,40 @@ const key = config.configAPI;
 //   }
 // });
 
-// Geocoding
-
-// const geoUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=pk.eyJ1Ijoia3hkYW5nIiwiYSI6ImNrMGhidGtrcTAwenEzcXBwZG4zcG1qcWoifQ.9UesO7UBWMh7i7IBsGybRw&limit=1`;
-// request({ url: geoUrl, json: true }, (error, response) => {
-//   if (error) {
-//     console.log("Unable to connect to service");
-//   } else if (response.body.features.length == 0) {
-//     console.log("Please search another term");
-//   } else {
-//     const latitude = response.body.features[0].center[1];
-//     const longitude = response.body.features[0].center[0];
-//     console.log(latitude, longitude);
-//   }
+// geocode("Mississauga", (error, data) => {
+//   console.log("Error", error);
+//   console.log("Data", data);
 // });
 
-const geocode = (address, callback) => {
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
-    address
-  )}.json?access_token=pk.eyJ1Ijoia3hkYW5nIiwiYSI6ImNrMGhidGtrcTAwenEzcXBwZG4zcG1qcWoifQ.9UesO7UBWMh7i7IBsGybRw&limit=1`;
+//
+// Goal: Create a reusable function for getting the forecast
+//
+// 1. Setup the "forecast" function in utils/forecast.js
+// 2. Require the function in app.js and call it as shown below
+// 3. The forecast function should have three potential calls to callback:
+//    - Low level error, pass string for error
+//    - Coordinate error, pass string for error
+//    - Success, pass forecast string for data (same format as from before)
+
+const forecast = (lat, long, callback) => {
+  const url = `https://api.darksky.net/forecast/${key}/${lat},${long}?units=si`;
 
   request({ url: url, json: true }, (error, response) => {
     if (error) {
-      callback("Unable to connect to location services", undefined);
-    } else if (response.body.features.length === 0) {
-      callback("Unable to find location, try another search", undefined);
+      callback("Unable to connect to location", undefined);
+    } else if (response.body.error) {
+      callback("Unable to find location", undefined);
     } else {
-      callback(undefined, {
-        latitude: response.body.features[0].center[1],
-        longitude: response.body.features[0].center[0],
-        location: response.body.features[0].place_name
-      });
+      const currently = response.body.currently;
+      callback(
+        undefined,
+        `${response.body.daily.data[0].summary} It is currently ${currently.temperature} degrees outside. There is a ${currently.precipProbability}% chance of rain`
+      );
     }
   });
 };
 
-geocode("Philadelphia New York", (error, data) => {
+forecast(-75.7088, 44.1545, (error, data) => {
   console.log("Error", error);
   console.log("Data", data);
 });
